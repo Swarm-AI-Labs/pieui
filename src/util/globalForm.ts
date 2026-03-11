32c093a6ce0e0112d7d85a5319c160335812b1f0
George Kasparyants <gg_plastt@mail.ru>
2026-03-11 11:44:42 +0300

    submit global form

--- src/util/globalForm.ts
+++ src/util/globalForm.ts
@@ -1,5 +1,6 @@
 export const submitGlobalForm = () => {
-    const formElement = document.getElementById('piedata_global_form') as HTMLFormElement
+    const formElement = document.getElementById(
+        'piedata_global_form'
+    ) as HTMLFormElement
     formElement && formElement.submit()
 }
-
